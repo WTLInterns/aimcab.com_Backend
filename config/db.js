67471e4bdba1e06e70cd0fef6ff911e1187942d5
Hrushi-00 <rushiskapse@gmail.com>
2025-04-24 13:55:28 +0530

    Removed node_modules from tracking and added to .gitignore

--- config/db.js
+++ config/db.js
@@ -1,37 +1,16 @@
-// // db.js
-// const mysql = require('mysql');
 
-// // Create the connection
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root123',
-//   database: 'demo',
-// });
-
-// // Connect to the database
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL: ', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database!');
-// });
-
-// // connection.query("Select * from oneway",(err,result)=>{
-// //   console.warn("result",result)
-// // })
-// module.exports = connection;
 
 
 const mysql = require('mysql2/promise'); // ✅ Use mysql2 with promise support
+require('dotenv').config();  // ✅ MUST be at the top!
+
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'demo',
+    host: process.env.DB_URI,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
